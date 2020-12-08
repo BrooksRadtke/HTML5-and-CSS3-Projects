ddf927fbe77f33e9c1814e13b730087fcc46a373
Brooks Radtke <brooks@brooksradtke.com>
2020-12-08 11:57:03 -0800

    Push Changes to repository

--- Project/JS/one_page_website.js
+++ Project/JS/one_page_website.js
@@ -25,14 +25,14 @@ function currentSlide(n)
     showSlides(slideIndex = n);
 }
 
-function showSlides()
+function showSlides(n)
 {
     var i;
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("photo"); //I used the name of the class
     var captionText = document.getElementById("caption");
     if(n > slides.length) {slideIndex = 1}
-    if(n < 1) {slideIndex = slide.length}
+    if(n < 1) {slideIndex = slides.length}
     for(i = 0; i < slides.length; i++)
     {
         slides[i].style.display = "none";
